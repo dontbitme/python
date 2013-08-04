50f65f781fd119ece2a042cfd573db2831f541d1
dontbitme <konrad.bit@o2.pl>
2013-08-04 10:25:24 +0200

    Update słownik.py

--- słownik.py
+++ słownik.py
@@ -1,17 +1,21 @@
-def slownik(zdanie):
+   lista =  zdanie.split(';;')
+   library = {1:lista[0].split(":"), 2:lista[1].split(":"), 3:lista[2].split(":")}
+   klucze = library.keys()
 
 
-    zdanie.split(':')
-    library= {1:{'imie', 'nazwisko', 'ulica', 'mieszkanie'}}
-    for i in zdanie:
-        if i != ';;':
-            library[1]['imie']=i        # TO JEST ZLE ALE POWINNO COS WYSWIETLIC 
+   print klucze
+   
+   print(library[1])
+   
+    
+
+               
             
             
             
 
-
-    print library
 
     
-slownik('Jan:Kowalski:Szczęśliwa:12/5;;2:Adam:Szczęsny:Zachlapana:10;;')  
+
+    
+slownik('1:Jan:Kowalski:Szczęśliwa:12/5;;2:Adam:Szczęsny:Zachlapana:10;;')      
