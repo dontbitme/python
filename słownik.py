6a747e62de11ec444094e4282aa9b3db6a47ec5a
dontbitme <konrad.bit@o2.pl>
2013-08-10 11:31:32 +0200

    Update słownik.py

--- słownik.py
+++ słownik.py
@@ -1,18 +1,30 @@
 def slownik(zdanie):
 
- #  num = 1
-  # index = 0
+   num = 1
+   index = 0
    library = {}
+   library2 = {}
    lista =  zdanie.split(";;")
    for i in lista:
-       list1 = i.split(":")
-       e = ' '.join(list1)
-       library["cos"] = e
-       
-       print library
-   
-   
+       if i == num:
+           
+           list1 = i.split(":")
+           library["id"] = list1[0]
+           library["imie"] = list1[1]
+           library["nazwisko"] = list1[2]
+
+           print library
+           
+           num + 2
+       else:
+           
+           list1 = i.split(":")
+           library2["id"] = list1[0]
+           library2["imie"] = list1[1]
+           library2["nazwisko"] = list1[2]
+
+           print library2
 
 
     
-slownik('1:Jan:Kowalski:Szczęśliwa:12/5;;2:Adam:Szczęsny:Zachlapana:10;;')    
+slownik('1:Jan:Kowalski:Szczęśliwa:12/5;;2:Adam:Szczęsny:Zachlapana:10;;')      
