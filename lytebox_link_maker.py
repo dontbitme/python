bc4d6499edd3e5dc5cb33e48ffafafb7122db502
dontbitme <konrad.bit@o2.pl>
2013-08-26 15:26:33 +0200

    Update lytebox_link_maker.py

--- lytebox_link_maker.py
+++ lytebox_link_maker.py
@@ -5,7 +5,6 @@ def lytebox_link_maker(folder_location):
     
     lista = os.listdir(folder_location)
     for i in lista:
-        print "<a href=\"images/"+i+" class=\"lytebox\" data-lyte-options=\"group:vacation\" data-title=\"Some Title\">Nazwa</a>"
+        print "<a href=\"images/"+i+"\" class=\"lytebox\" data-lyte-options=\"group:vacation\" data-title=\"Some Title\">Nazwa</a>"
     
-
 lytebox_link_maker("c:\\users\\Konrad\\desktop\\foty\\")
