0becaf291ee6449d59d3503694cc6c086aa92a72
dontbitme <konrad.bit@o2.pl>
2013-08-26 15:22:36 +0200

    Create lytebox_link_maker.py

---
+++ lytebox_link_maker.py
@@ -0,0 +1,11 @@
+import os
+
+
+def lytebox_link_maker(folder_location):
+    
+    lista = os.listdir(folder_location)
+    for i in lista:
+        print "<a href=\"images/"+i+" class=\"lytebox\" data-lyte-options=\"group:vacation\" data-title=\"Some Title\">Nazwa</a>"
+    
+
+lytebox_link_maker("c:\\users\\Konrad\\desktop\\foty\\")
