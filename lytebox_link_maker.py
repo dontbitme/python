84df40044fa98ffcaad9e3430ec4076275f95814
dontbitme <konrad.bit@o2.pl>
2013-08-27 20:46:28 +0200

    Update lytebox_link_maker.py

--- lytebox_link_maker.py
+++ lytebox_link_maker.py
@@ -1,8 +1,10 @@
 import os
 
 def lytebox_link_maker(folder_location):
-
+    cos = ".jpg", ".gif"
     for i in os.listdir(folder_location):
-        print  '<a href="images"'+ i +'" class="lytebox" data-lyte-options="group:vacation" data-title="Some Title">Nazwa</a>'
-    
+        (shortname, extension) = os.path.splitext(i)
+        if extension == cos:
+            print  '<a href="images"'+ i +'" class="lytebox" data-lyte-options="group:vacation" data-title="Some Title">Nazwa</a>'
+        
 lytebox_link_maker("c:\\users\\Konrad\\desktop\\foty\\")
