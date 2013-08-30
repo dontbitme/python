45ab36613eefd1f7ea2bd4745b23792eb429dd55
dontbitme <konrad.bit@o2.pl>
2013-08-30 15:40:04 +0200

    Update lytebox_link_maker.py

--- lytebox_link_maker.py
+++ lytebox_link_maker.py
@@ -7,6 +7,8 @@ def lytebox_link_maker(folder_location):
         for l in cos:
             if extension == l:
                 print  '<a href="images"'+ i +'" class="lytebox" data-lyte-options="group:vacation" data-title="Some Title">Nazwa</a>'
-
+            else:
+                print "Plik nie jest obrazem."
 lytebox_link_maker("c:\\users\\Konrad\\desktop\\foty\\")
+
 
