452f9112ce56d86014a011e47b070fcda900351a
dontbitme <konrad.bit@o2.pl>
2013-09-01 12:26:26 +0200

    Update lytebox_link_maker.py

--- lytebox_link_maker.py
+++ lytebox_link_maker.py
@@ -4,9 +4,12 @@ def lytebox_link_maker(folder_location):
     for i in os.listdir(folder_location):
         (shortname, extension) = os.path.splitext(i)
         cos = [".jpg",".gif", ".png"]
+        
         if extension in cos:
-            print  '<a href="images"'+ i +'" class="lytebox" data-lyte-options="group:vacation" data-title="Some Title">Nazwa</a>'
+            plik = open("tekst.log", "w")
+            plik.write( '<a href="images"'+ i +'" class="lytebox" data-lyte-options="group:vacation" data-title="Some Title">Nazwa</a>')
+            print open("tekst.log").read()
         else:
-            print "Plik "+i+"nie jest obrazem."
+            print "Plik "+i+" nie jest obrazem."
 lytebox_link_maker("c:\\users\\Konrad\\desktop\\foty\\")
 
