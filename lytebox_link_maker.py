1c6f67a60df1eff8298f73f0eab940f854959441
dontbitme <konrad.bit@o2.pl>
2013-09-01 16:16:03 +0200

    Update lytebox_link_maker.py

--- lytebox_link_maker.py
+++ lytebox_link_maker.py
@@ -3,15 +3,15 @@ import os
 def lytebox_link_maker(folder_location):
     for i in os.listdir(folder_location):
         (shortname, extension) = os.path.splitext(i)
-        cos = [".jpg",".gif", ".png"]
-        plik = open("tekst.log", "w")        
+        cos = [".jpg",".gif", ".png"]   
+        plik = open("tekst.log", "w")     
         if extension in cos:
-            plik2 = open("tekst.log")
-            plik2.write( '<a href="images"'+ i +'" class="lytebox" data-lyte-options="group:vacation" data-title="Some Title">Nazwa</a>\n')
-            plik2.close()
+            open("tekst.log")
+            plik.write( '<a href="images"'+ i +'" class="lytebox" data-lyte-options="group:vacation" data-title="Some Title">Nazwa</a>\n')
+            plik.close() 
         else:
             print "Plik "+i+" nie jest obrazem."
-            
-        print plik2.read()
+           
+        print plik.read()
 lytebox_link_maker("c:\\users\\Konrad\\desktop\\foty\\")
 
